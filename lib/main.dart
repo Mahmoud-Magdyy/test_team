308b5ccd937b46c914c56f41d67009fbf1dbd356
Mahmoud <mahmoudmagdy20002@gmail.com>
2023-10-14 21:42:49 +0300

    last

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 //splash from mahmoud 
-
-//steps 
-//1. checkout master 
-//2. update master
-//3. create branch 
-//4. code the task 
-//5. commit 
-//6. back to master
-//7. update master
-//8. merge master with my branch
-//9. push to github
-//10. pull request from google  
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
